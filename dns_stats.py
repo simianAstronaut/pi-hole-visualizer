ae1dd03423666faf2d5f9b94b766b6cd59598f73
Sam Lindley <paranoidAndroid@protonmail.com>
2018-02-28 22:12:15 -0800

    Added option to color code chart based on ad block percentage

--- dns_stats.py
+++ dns_stats.py
@@ -33,7 +33,7 @@ def dns_request(local_address):
     #aggregate data into hourly intervals
     for i in range(len(domain_info_mins)):
         if i > 0 and i % 6 == 0:
-            domain_info_hourly.append([domains, ads])
+            domain_info_hourly.append([domains, (ads / domains) * 100])
             domains = 0
             ads = 0
         domains += domain_info_mins[i][0]
@@ -47,6 +47,7 @@ def dns_request(local_address):
 #color codes hourly interval based on relative level of dns traffic
 def color_dict(level):
     return {
+        0 : (0, 0, 255),
         1 : (0, 128, 255),
         2 : (0, 255, 255),
         3 : (255, 128, 0),
@@ -57,44 +58,58 @@ def color_dict(level):
         8 : (255, 0, 0),
     }[level]
 
-def generate_chart(data, flag):
-    domain_info_chart = []
-    minimum = data[0][0]
-    maximum = data[0][0]
+def generate_chart(data, color):
+    info_chart = []
+    domain_min = data[0][0]
+    domain_max = data[0][0]
+    ad_min = data[0][1]
+    ad_max = data[0][1]
 
     #calculate minimum, maximum, and interval values to scale graph appropriately
     for hour in data:
-        if hour[0] > maximum:
-            maximum = hour[0]
-        elif hour[0] < minimum:
-            minimum = hour[0]
-    chart_interval = (maximum - minimum) / 8
+        if hour[0] > domain_max:
+            domain_max = hour[0]
+        elif hour[0] < domain_min:
+            domain_min = hour[0]
+
+        if hour[1] > ad_max:
+            ad_max = hour[1]
+        elif hour[1] < ad_min:
+            ad_min = hour[1]
+
+    domain_interval = (domain_max - domain_min) / 8
+    ad_interval = (ad_max - ad_min) / 8
 
     #append final scaled values to new list
     for hour in data:
-        domain_info_chart.append(int((hour[0] - minimum) / chart_interval))
-    domain_info_chart = list(reversed(domain_info_chart[:8]))
+        info_chart.append([int((hour[0] - domain_min) / domain_interval), \
+                                  int((hour[1] - ad_min) / ad_interval)])
+    info_chart = list(reversed(info_chart[:8]))
 
     sense = SenseHat()
     sense.clear()
 
     #set pixel values on rgb display
     for col in range(0, 8):
-        if domain_info_chart[col] > 0:
-            for row in range(0, domain_info_chart[col]):
-                #if flag not set, default to color red for all values
-                sense.set_pixel(row, col, color_dict(domain_info_chart[col]) if flag else \
-                                (255, 0, 0))
+        if info_chart[col][0] > 0:
+            for row in range(0, info_chart[col][0]):
+                #if color not set, default to color red for all values
+                if color == 'traffic':
+                    sense.set_pixel(row, col, color_dict(info_chart[col][0]))
+                elif color == 'ads':
+                    sense.set_pixel(row, col, color_dict(info_chart[col][1]))
+                else:
+                    sense.set_pixel(row, col, (255, 0, 0))
 
 def main():
     parser = optparse.OptionParser(description="Generates a chart to display network traffic on the sense-hat RGB display")
-    parser.add_option('-c', action="store_true", dest="color_flag", help="use color to indicate level of network traffic")
+    parser.add_option('-c', action="store", type="choice", dest="color", choices=["traffic", "ads"], help="use color to indicate level of network traffic or percentage of ads blocked")
     parser.add_option('-a', action="store", dest="local_address", type="string", help="specify address of DNS server, defaults to localhost")
-    parser.set_defaults(color_flag=False, local_address='127.0.0.1')
+    parser.set_defaults(color=None, local_address='127.0.0.1')
     opt, args = parser.parse_args()
 
     domain_info_hourly = dns_request(opt.local_address)
-    generate_chart(domain_info_hourly, opt.color_flag)
+    generate_chart(domain_info_hourly, opt.color)
 
 if __name__ == '__main__':
     main()
