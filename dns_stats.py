2e9d60219e4630bdf9fd8abce3cffb909b86eae2
Sam Lindley <spaceChimp@protonmail.com>
2018-03-17 13:17:53 -0700

    Added option to specify time interval

--- dns_stats.py
+++ dns_stats.py
@@ -13,6 +13,8 @@ import time
 import urllib.request
 from sense_hat import SenseHat
 
+sense = SenseHat()
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -20,61 +22,6 @@ handler = logging.FileHandler('/var/log/pihole-visualizer.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-
-def dns_request(address):
-    domain_info_hourly = []
-    key_count = 0
-    domains = 0
-    ads = 0
-    attempts = 0
-
-    #retrieve and decode json data from server
-    while True:
-        try:
-            if attempts == 0:
-                logger.info('Initiating connection with server.')
-                print('Initiating connection with server.')
-            with urllib.request.urlopen("http://%s/admin/api.php?overTimeData10mins" % \
-            address) as url:
-                attempts += 1
-                data = json.loads(url.read().decode())
-                break
-        except json.decoder.JSONDecodeError:
-            if attempts < 300:
-                time.sleep(1)
-                continue
-            else:
-                logger.error('Exceeded max attempts to connect with server.')
-                print('Error: Exceeded max attempts to connect with server.')
-                sys.exit(1)
-        except urllib.error.URLError:
-            logger.error('Invalid address for DNS server.')
-            print("Error: Invalid address for DNS server. Try again.")
-            sys.exit(1)
-
-    if 'domains_over_time' not in data or 'ads_over_time' not in data:
-        logger.error('Invalid data returned from server. Ensure pihole-FTL service is running.')
-        print('Error: Invalid data returned from server. Ensure pihole-FTL service is running.')
-        sys.exit(1)
-
-    logger.info('Successful connection with server.')
-    print('Successful connection with server.')
-
-    #sort and reverse data so that latest time intervals appear first in list
-    for key in sorted(data['domains_over_time'].keys(), reverse=True):
-        #aggregate data into hourly intervals
-        if key_count > 0 and key_count % 6 == 0:
-            domain_info_hourly.append([domains, (ads / domains) * 100 if domains > 0 else 0])
-            domains = 0
-            ads = 0
-        domains += data['domains_over_time'][key]
-        ads += data['ads_over_time'][key]
-        key_count += 1
-    
-    #extract a slice of the previous 24 hours
-    domain_info_hourly = domain_info_hourly[:24]
-
-    return domain_info_hourly
 
 #color code hourly interval
 def color_dict(level):
@@ -89,6 +36,87 @@ def color_dict(level):
         7 : (255, 128, 0),
         8 : (255, 0, 0),
     }[level]
+
+def api_request(address):
+    attempts = 0
+
+    #retrieve and decode json data from server
+    while True:
+        try:
+            if attempts == 0:
+                logger.info('Initiating connection with server.')
+                print('Initiating connection with server.')
+            with urllib.request.urlopen("http://%s/admin/api.php?overTimeData10mins" % \
+            address) as url:
+                attempts += 1
+                raw_data = json.loads(url.read().decode())
+                break
+        except json.decoder.JSONDecodeError:
+            if attempts < 300:
+                time.sleep(1)
+                continue
+            else:
+                logger.error('Exceeded max attempts to connect with server.')
+                print('Error: Exceeded max attempts to connect with server.')
+                sys.exit(1)
+        except urllib.error.URLError:
+            logger.error('Invalid address for DNS server.')
+            print("Error: Invalid address for DNS server. Try again.")
+            sys.exit(1)
+
+    if 'domains_over_time' not in raw_data or 'ads_over_time' not in raw_data:
+        logger.error('Invalid data returned from server. Ensure pihole-FTL service is running.')
+        print('Error: Invalid data returned from server. Ensure pihole-FTL service is running.')
+        sys.exit(1)
+
+    logger.info('Successful connection with server.')
+    print('Successful connection with server.')
+    
+    return raw_data
+
+def organize_data(raw_data, interval):
+    clean_data = []
+    key_count = 0
+    domains = 0
+    ads = 0
+    
+	#sort and reverse data so that latest time intervals appear first in list
+    for key in sorted(raw_data['domains_over_time'].keys(), reverse=True):
+        if interval == 10:
+            domains = raw_data['domains_over_time'][key]
+            ads = raw_data['ads_over_time'][key]
+            clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+        else:
+            if interval == 30:
+                if key_count > 0 and key_count % 3 == 0:
+                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    domains = 0
+                    ads = 0
+            elif interval == 60:
+                if key_count > 0 and key_count % 6 == 0:
+                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    domains = 0
+                    ads = 0
+            elif interval == 120:
+                if key_count > 0 and key_count % 12 == 0:
+                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    domains = 0
+                    ads = 0
+            domains += raw_data['domains_over_time'][key]
+            ads += raw_data['ads_over_time'][key]
+            key_count += 1
+    
+    #extract a slice of the previous 24 hours
+    if interval == 10:
+        clean_data = clean_data[:144]
+    elif interval == 30:
+        clean_data = clean_data[:48]
+    elif interval == 60:
+        clean_data = clean_data[:24]
+    elif interval == 120:
+        clean_data = clean_data[:12]
+
+    return clean_data
 
 def generate_chart(data, color, ripple, orientation, lowlight):
     info_chart = []
@@ -118,7 +146,6 @@ def generate_chart(data, color, ripple, orientation, lowlight):
                            else 0, int((hour[1] - ad_min) / ad_interval) if ad_interval > 0 else 0])
     info_chart = list(reversed(info_chart[:8]))
 
-    sense = SenseHat()
     sense.clear()
 
     sense.set_rotation(orientation)
@@ -149,6 +176,8 @@ def generate_chart(data, color, ripple, orientation, lowlight):
 def main():
     parser = argparse.ArgumentParser(description="Generates a chart to display network traffic \
                                      on the sense-hat RGB display")
+    parser.add_argument('-i', '--interval', action="store", choices=[10, 30, 60, 120], \
+                        type=int, default='60', help="specify interval time in minutes")                                 
     parser.add_argument('-c', '--color', action="store", choices=["traffic", "ads", "alternate"], \
                         help="specify 'traffic' to display level of network traffic, 'ads' to \
                         display percentage of ads blocked, or 'alternate' to switch between both")
@@ -168,14 +197,16 @@ def main():
         color = args.color
 
     while True:
-        domain_info_hourly = dns_request(args.address)
+        raw_data = api_request(args.address)
+        clean_data = organize_data(raw_data, args.interval)
+        
         if args.color == 'alternate':
             for i in range(0, 15):
                 color = 'ads' if color == 'traffic' else 'traffic'
-                generate_chart(domain_info_hourly, color, args.ripple, args.orientation, args.lowlight)
+                generate_chart(clean_data, color, args.ripple, args.orientation, args.lowlight)
                 time.sleep(2)
         else:
-            generate_chart(domain_info_hourly, color, args.ripple, args.orientation, args.lowlight)
+            generate_chart(clean_data, color, args.ripple, args.orientation, args.lowlight)
             time.sleep(30)
 
 if __name__ == '__main__':
