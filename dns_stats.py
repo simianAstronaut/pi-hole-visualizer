449e1bc8fe01957e38ddd07589e5487619fe052b
Sam Lindley <paranoidAndroid@protonmail.com>
2018-04-05 20:25:08 -0700

    Added spiral graph to display overall ad block percentage

--- dns_stats.py
+++ dns_stats.py
@@ -6,6 +6,7 @@ By Sam Lindley, 2/21/2018
 '''
 
 import argparse
+from itertools import cycle
 import json
 import logging
 import os
@@ -15,6 +16,7 @@ import urllib.request
 from sense_hat import SenseHat
 
 SENSE = SenseHat()
+RIPPLE_SPEED = 0.025
 
 if os.geteuid() == 0:
     LOGGER = logging.getLogger(__name__)
@@ -26,20 +28,21 @@ if os.geteuid() == 0:
     LOGGER.addHandler(HANDLER)
 
 
-def joystick_up(color_mode):
-    color_options = ('basic', 'traffic', 'ads', 'alternate')
-    color_index = color_options.index(color_mode)
+def joystick_up_pushed(color):
+    color_options = ('basic', 'traffic', 'ads')
+    color_index = color_options.index(color)
 
-    if color_index == 3:
+    if color_index == 2:
         color_index = 0
     else:
         color_index += 1
 
-    color_mode = color_options[color_index]
+    color = color_options[color_index]
 
-    return color_mode
+    return color
 
-def joystick_right(interval):
+
+def joystick_right_pushed(interval):
     interval_options = (10, 30, 60, 120)
     interval_index = interval_options.index(interval)
 
@@ -52,7 +55,8 @@ def joystick_right(interval):
 
     return interval
 
-def joystick_down(lowlight):
+
+def joystick_down_pushed(lowlight):
     if lowlight:
         lowlight = False
     else:
@@ -60,7 +64,8 @@ def joystick_down(lowlight):
 
     return lowlight
 
-def joystick_left(orientation):
+
+def joystick_left_pushed(orientation):
     orientation_options = (0, 90, 180, 270)
     orientation_index = orientation_options.index(orientation)
 
@@ -73,13 +78,6 @@ def joystick_left(orientation):
 
     return orientation
 
-def joystick_middle(ripple):
-    if ripple:
-        ripple = False
-    else:
-        ripple = True
-
-    return ripple
 
 def joystick_middle_held():
     if os.geteuid() == 0:
@@ -90,7 +88,7 @@ def joystick_middle_held():
 
     sys.exit()
 
-#color code interval
+
 def color_dict(level):
     return {
         0 : (0, 0, 255),
@@ -103,6 +101,7 @@ def color_dict(level):
         7 : (255, 128, 0),
         8 : (255, 0, 0),
     }[level]
+
 
 def api_request(address):
     if not hasattr(api_request, "initial_connection"):
@@ -117,7 +116,7 @@ def api_request(address):
                 if os.geteuid() == 0:
                     LOGGER.info('Initiating connection with server.')
                 print('Initiating connection with server.')
-            with urllib.request.urlopen("http://%s/admin/api.php?overTimeData10mins" % \
+            with urllib.request.urlopen("http://%s/admin/api.php?summary&overTimeData10mins" % \
             address) as url:
                 attempts += 1
                 raw_data = json.loads(url.read().decode())
@@ -152,8 +151,9 @@ def api_request(address):
 
     return raw_data
 
-def organize_data(raw_data, interval):
-    clean_data = []
+
+def generate_interval_data(raw_data, interval):
+    interval_data = []
     domains = 0
     ads = 0
 
@@ -162,21 +162,21 @@ def organize_data(raw_data, interval):
         if interval == 10:
             domains = raw_data['domains_over_time'][key]
             ads = raw_data['ads_over_time'][key]
-            clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+            interval_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
         else:
             if interval == 30:
                 if counter > 0 and counter % 3 == 0:
-                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    interval_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
                     domains = 0
                     ads = 0
             elif interval == 60:
                 if counter > 0 and counter % 6 == 0:
-                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    interval_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
                     domains = 0
                     ads = 0
             elif interval == 120:
                 if counter > 0 and counter % 12 == 0:
-                    clean_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
+                    interval_data.append([domains, (ads / domains) * 100 if domains > 0 else 0])
                     domains = 0
                     ads = 0
             domains += raw_data['domains_over_time'][key]
@@ -184,25 +184,61 @@ def organize_data(raw_data, interval):
 
     #extract a slice of the previous 24 hours
     if interval == 10:
-        clean_data = clean_data[:144]
+        interval_data = interval_data[:144]
     elif interval == 30:
-        clean_data = clean_data[:48]
+        interval_data = interval_data[:48]
     elif interval == 60:
-        clean_data = clean_data[:24]
+        interval_data = interval_data[:24]
     elif interval == 120:
-        clean_data = clean_data[:12]
+        interval_data = interval_data[:12]
 
-    return clean_data
+    return interval_data
 
-def generate_chart(clean_data, color, ripple, orientation, lowlight):
+
+def spiral_graph(block_percentage, orientation, lowlight, x=3, y=3):
+    grid_size = 64
+    dx = 0
+    dy = 1
+    pivot_index = 0
+    pivot_point = 1
+
+    grid_units = int(grid_size * block_percentage)
+
+    SENSE.clear()
+    SENSE.set_rotation(orientation)
+    SENSE.low_light = lowlight
+
+    for i in range(grid_size):
+        if i < grid_units:
+            SENSE.set_pixel(x, 7 - y, (255, 0, 0))
+        else:
+            SENSE.set_pixel(x, 7 - y, (0, 0, 255))
+
+        time.sleep(RIPPLE_SPEED)
+
+        if pivot_index == pivot_point:
+            if dx == 0:
+                dx, dy = dy, dx
+                pivot_index = 0
+            elif dy == 0:
+                dx, dy = dy, -dx
+                pivot_index = 0
+                pivot_point += 1
+
+        x += dx
+        y += dy
+        pivot_index += 1
+
+
+def bar_chart(interval_data, color, orientation, lowlight):
     info_chart = []
-    domain_min = clean_data[0][0]
-    domain_max = clean_data[0][0]
-    ad_min = clean_data[0][1]
-    ad_max = clean_data[0][1]
+    domain_min = interval_data[0][0]
+    domain_max = interval_data[0][0]
+    ad_min = interval_data[0][1]
+    ad_max = interval_data[0][1]
 
     #calculate minimum, maximum, and interval values to scale graph appropriately
-    for i in clean_data:
+    for i in interval_data:
         if i[0] > domain_max:
             domain_max = i[0]
         elif i[0] < domain_min:
@@ -217,7 +253,7 @@ def generate_chart(clean_data, color, ripple, orientation, lowlight):
     ad_interval = (ad_max - ad_min) / 8
 
     #append scaled values to new list
-    for i in clean_data:
+    for i in interval_data:
         info_chart.append([int((i[0] - domain_min) / domain_interval) if domain_interval > 0 \
                            else 0, int((i[1] - ad_min) / ad_interval) if ad_interval > 0 else 0])
     info_chart = list(reversed(info_chart[:8]))
@@ -233,16 +269,63 @@ def generate_chart(clean_data, color, ripple, orientation, lowlight):
                 #if color not set, default to red for all values
                 if color == 'traffic':
                     SENSE.set_pixel(row, 7 - col, color_dict(info_chart[row][0]))
-                    if ripple:
-                        time.sleep(0.025)
+                    time.sleep(RIPPLE_SPEED)
                 elif color == 'ads':
                     SENSE.set_pixel(row, 7 - col, color_dict(info_chart[row][1]))
-                    if ripple:
-                        time.sleep(0.025)
+                    time.sleep(RIPPLE_SPEED)
                 elif color == 'basic':
                     SENSE.set_pixel(row, 7 - col, (255, 0, 0))
-                    if ripple:
-                        time.sleep(0.025)
+                    time.sleep(RIPPLE_SPEED)
+
+
+def event_loop(args):
+    modes = ('bar', 'spiral')
+    cycler = cycle(modes)
+
+    while True:
+        joystick_event = False
+
+        raw_data = api_request(args.address)
+        block_percentage = float(raw_data['ads_percentage_today']) / 100
+        interval_data = generate_interval_data(raw_data, args.interval)
+
+        for _ in range(0, 15):
+            mode = next(cycler)
+            if  mode == 'bar':
+                bar_chart(interval_data, args.color, args.orientation, args.lowlight)
+            elif mode == 'spiral':
+                spiral_graph(block_percentage, args.orientation, args.lowlight)
+
+            for _ in range(0, 2):
+                events = SENSE.stick.get_events()
+                if events:
+                    joystick_event = True
+                    last_event = events[-1]
+                    if last_event.direction == 'up':
+                        args.color = joystick_up_pushed(args.color)
+                        print("Color mode switched to '%s'." % args.color.capitalize())
+                        break
+                    elif last_event.direction == 'right':
+                        args.interval = joystick_right_pushed(args.interval)
+                        print("Time interval switched to %d minutes." % args.interval)
+                        break
+                    elif last_event.direction == 'down':
+                        args.lowlight = joystick_down_pushed(args.lowlight)
+                        print("Low-light mode", "enabled." if args.lowlight else \
+                              "disabled.")
+                        break
+                    elif last_event.direction == 'left':
+                        args.orientation = joystick_left_pushed(args.orientation)
+                        print("Orientation switched to %d degrees." % args.orientation)
+                        break
+                    elif last_event.direction == 'middle' and last_event.action == 'held':
+                        joystick_middle_held()
+
+                time.sleep(1)
+
+            if joystick_event:
+                break
+
 
 def main():
     parser = argparse.ArgumentParser(description="Generates a chart to display network traffic \
@@ -250,13 +333,10 @@ def main():
 
     parser.add_argument('-i', '--interval', action="store", choices=[10, 30, 60, 120], \
                         type=int, default='60', help="specify interval time in minutes")
-    parser.add_argument('-c', '--color', action="store", choices=['basic', 'traffic', 'ads', \
-                        'alternate'], default='basic', help="specify 'basic' to generate the \
-                        default red chart, 'traffic' to represent the level of network traffic, \
-                        'ads' to represent the percentage of ads blocked, or 'alternate' to \
-                        switch between traffic level and ad block percentage")
-    parser.add_argument('-r', '--ripple', action="store_true", help="this option generates a \
-                        ripple effect when producing the chart")
+    parser.add_argument('-c', '--color', action="store", choices=['basic', 'traffic', 'ads'], \
+                        default='basic', help="specify 'basic' to generate the default red \
+                        chart, 'traffic' to represent the intensity of network traffic, or \
+                        'ads' to represent the percentage of ads blocked")
     parser.add_argument('-a', '--address', action="store", default='127.0.0.1', help="specify \
                         address of DNS server, defaults to localhost")
     parser.add_argument('-o', '--orientation', action="store", choices=[0, 90, 180, 270], \
@@ -265,91 +345,9 @@ def main():
                         light mode for use in dark environments")
 
     args = parser.parse_args()
-    color_mode = args.color
 
-    if color_mode == 'alternate':
-        color = 'traffic'
-    else:
-        color = color_mode
+    event_loop(args)
 
-    while True:
-        joystick_event = False
-
-        raw_data = api_request(args.address)
-        clean_data = organize_data(raw_data, args.interval)
-
-        if color_mode == 'alternate':
-            for i in range(0, 15):
-                color = 'ads' if color == 'traffic' else 'traffic'
-                generate_chart(clean_data, color, args.ripple, args.orientation, args.lowlight)
-
-                for i in range(0, 2):
-                    events = SENSE.stick.get_events()
-                    if events:
-                        joystick_event = True
-                        last_event = events[-1]
-                        if last_event.action == 'held' and last_event.direction == 'middle':
-                            joystick_middle_held()
-                        else:
-                            if last_event.direction == 'up':
-                                color_mode = joystick_up(color_mode)
-                                print("Color mode switched to '%s'." % color_mode.capitalize())
-                                break
-                            elif last_event.direction == 'right':
-                                args.interval = joystick_right(args.interval)
-                                print("Time interval switched to %d minutes." % args.interval)
-                                break
-                            elif last_event.direction == 'down':
-                                args.lowlight = joystick_down(args.lowlight)
-                                print("Low-light mode", "enabled." if args.lowlight else \
-                                      "disabled.")
-                                break
-                            elif last_event.direction == 'left':
-                                args.orientation = joystick_left(args.orientation)
-                                print("Orientation switched to %d degrees." % args.orientation)
-                                break
-                            elif last_event.direction == 'middle':
-                                args.ripple = joystick_middle(args.ripple)
-                                print("Ripple mode", "enabled." if args.ripple else "disabled.")
-                                break
-
-                    time.sleep(1)
-
-                if joystick_event:
-                    break
-        else:
-            color = color_mode
-            generate_chart(clean_data, color, args.ripple, args.orientation, args.lowlight)
-
-            for i in range(0, 30):
-                events = SENSE.stick.get_events()
-                if events:
-                    last_event = events[-1]
-                    if last_event.action == 'held' and last_event.direction == 'middle':
-                        joystick_middle_held()
-                    else:
-                        if last_event.direction == 'up':
-                            color_mode = joystick_up(color_mode)
-                            print("Color mode switched to '%s'." % color_mode.capitalize())
-                            break
-                        elif last_event.direction == 'right':
-                            args.interval = joystick_right(args.interval)
-                            print("Time interval switched to %d minutes." % args.interval)
-                            break
-                        elif last_event.direction == 'down':
-                            args.lowlight = joystick_down(args.lowlight)
-                            print("Low-light mode", "enabled." if args.lowlight else "disabled.")
-                            break
-                        elif last_event.direction == 'left':
-                            args.orientation = joystick_left(args.orientation)
-                            print("Orientation switched to %d degrees." % args.orientation)
-                            break
-                        elif last_event.direction == 'middle':
-                            args.ripple = joystick_middle(args.ripple)
-                            print("Ripple mode", "enabled." if args.ripple else "disabled.")
-                            break
-
-                time.sleep(1)
 
 if __name__ == '__main__':
     main()
